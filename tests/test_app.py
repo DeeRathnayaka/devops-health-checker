0f7e69ec9c1f94065b12e57aa4fed3468996f5e2
Your Name <you@example.com>
2026-08-18 15:29:01 +0530

    chore: added tests.yml

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from devops_health_checker import app
+
+
+def test_home_endpoint():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["service"] == "devops-health-checker"
+    assert data["status"] == "running"
